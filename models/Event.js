8c64a29dddf360d7a8dbfff8d0858af61d9dd9a3
mohamed <tfue2603@gmail.com>
2026-08-18 21:34:13 +0300

    Complete Event schema

--- models/Event.js
+++ models/Event.js
@@ -34,5 +34,33 @@ const eventSchema = new mongoose.Schema({
         type: String,
         enum: ["workshop", "social", "competition", "volunteering", "other"],
         required: true
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+
+    totalSlots: {
+        type: Number
+    },
+
+    status: {
+        type: String,
+        enum: ["open", "closed"],
+        required: true
+    },
+
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 });
+
+module.exports = mongoose.model("Event", eventSchema);
